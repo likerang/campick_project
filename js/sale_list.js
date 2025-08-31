5eec7c4bc7a452f9b3f26b68e2f72d3f44577060
likerang <arang9314@naver.com>
2025-08-31 18:25:51 +0900

    feat: 더보기 창 추가 style: 더보기 layout 및 효과 추가

--- js/sale_list.js
+++ js/sale_list.js
@@ -1,21 +1,38 @@
-$(function() {
-  $('.tab_menu a').click(function(e) {
+const tabs = document.querySelectorAll('.tab_menu a');
+const lists = document.querySelectorAll('[data-list]');
+const moreBtns = document.querySelectorAll('.more_btn');
+const bottomSheet = document.querySelector('.bottom_sheet');
+
+// 판매중/결제완료 list tab
+tabs.forEach(tab => {
+  tab.addEventListener('click', function(e) {
     e.preventDefault();
 
-    // 클릭된 탭 active 처리
-    $('.tab_menu a').removeClass('active');
-    $(this).addClass('active');
+    // 탭 active 변경
+    tabs.forEach(t => t.classList.remove('active'));
+    this.classList.add('active');
 
-    // 어떤 필터인지 확인
-    const filter = $(this).data('filter');
+    const filter = this.dataset.filter;
 
-    // 목록 토글
-    if(filter === 'selling'){
-      $('.product_list_wrapper_2col').removeClass('hidden');
-      $('.soldout_list_wrapper').addClass('hidden');
-    } else if(filter === 'soldout'){
-      $('.soldout_list_wrapper').removeClass('hidden');
-      $('.product_list_wrapper_2col').addClass('hidden');
-    }
+    lists.forEach(list => {
+      if (list.dataset.list === filter) {
+        list.classList.remove('hidden'); // 보여주기
+      } else {
+        list.classList.add('hidden'); // 숨기기
+      }
+    });
   });
+});
+
+// 더보기 btn
+moreBtns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    bottomSheet.classList.toggle('active');
+  });
+});
+
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.more_btn') && !e.target.closest('.bottom_sheet')) {
+    bottomSheet.classList.remove('active');
+  }
 });
