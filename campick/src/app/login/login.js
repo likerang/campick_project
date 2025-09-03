9b0885f496527a4be6398040d90333412bb8b658
yeonmi0818 <parkyunme@naver.com>
2025-09-03 17:39:04 +0900

    Update login.js

--- campick/src/app/login/login.js
+++ campick/src/app/login/login.js
@@ -3,7 +3,10 @@ import Link from 'next/link';
 
 import "../css/login.css";
 
-
+export const metadata = {
+  title: "Campick - 로그인",
+  description: "Welcome to Campick",
+};
 
 export default function login() {
   return (
