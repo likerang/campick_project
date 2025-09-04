525e019193a41164c8ea5cdae73c9b0e546f318c
kyt0830 <kyt910830@gmail.com>
2025-09-04 12:59:50 +0900

    feat addProd 폴더 생성

---
+++ src/app/addprod/page.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+import Image from "next/image";
+import Link from 'next/link';
+import styles from './page.module.css'
+
+export const metadata = {
+    title: "Campick - 상품등록",
+    description: "Welcome to Campick",
+};
+
+export default function AddProd() {
+    return (
+        <>
+            <h1>상품을 등록하는 곳 입니다.</h1>
+        </>
+    )
+}
