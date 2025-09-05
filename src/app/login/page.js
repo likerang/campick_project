d2ad319b92ec4421c6beec53f7570afa8e522450
yeonmi0818 <parkyunme@naver.com>
2025-09-05 12:07:34 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,9 +10,9 @@ export const metadata = {
 
 export default function Login() {
   return (
-    <div className="container">
-      <div className="logo_section">
-        <div className="logo">
+    <div className={styles.container}>
+      <div className={styles.logo_section}>
+        <div className={styles.logo}>
           <Image
             src="/images/logo_black.png"
             alt="campick logo"
@@ -20,51 +20,51 @@ export default function Login() {
             height={49}
           />
         </div>
-        <div className="subtitle">
-          캠핑 <span className="font_primary">중고거래</span>는 캠픽
+        <div className={styles.subtitle}>
+          캠핑 <span className={styles.font_primary}>중고거래</span>는 캠픽
           <br />
-          캠핑 <span className="font_primary">트렌드</span>도 캠픽
+          캠핑 <span className={styles.font_primary}>트렌드</span>도 캠픽
         </div>
       </div>
 
       <form>
-        <div className="form_group">
-          <label className="form_label">이메일</label>
-          <input type="email" className="form_input" placeholder="" />
+        <div className={styles.form_group}>
+          <label className={styles.form_label}>이메일</label>
+          <input type="email" className={styles.form_input} placeholder="" />
         </div>
 
-        <div className="form_group">
-          <label className="form_label">비밀번호</label>
-          <input type="password" className="form_input" placeholder="" />
+        <div className={styles.form_group}>
+          <label className={styles.form_label}>비밀번호</label>
+          <input type="password" className={styles.form_input} placeholder="" />
         </div>
 
-        <button type="submit" className="login_btn btn_normal">
+        <button type="submit" className={`${styles.login_btn} ${styles.btn_normal}`}>
           로그인
         </button>
 
-        <div className="links">
+        <div className={styles.links}>
           <Link href="#">이메일 찾기</Link>
           <Link href="#">비밀번호 찾기</Link>
         </div>
 
-        <div className="social_login">
-          <div className="login_title">
-            <div className="border"></div>
+        <div className={styles.social_login}>
+          <div className={styles.login_title}>
+            <div className={styles.border}></div>
             <span>간편 로그인</span>
-            <div className="border"></div>
+            <div className={styles.border}></div>
           </div>
-          <button type="button" className="social_btn naver_btn">
+          <button type="button" className={`${styles.social_btn} ${styles.naver_btn}`}>
             네이버로 로그인
           </button>
 
-          <button type="button" className="social_btn kakao_btn">
+          <button type="button" className={`${styles.social_btn} ${styles.kakao_btn}`}>
             카카오 로그인
           </button>
         </div>
       </form>
 
-      <div className="signup_section">
-        <div className="border"></div>
+      <div className={styles.signup_section}>
+        <div className={styles.border}></div>
         <Link href="/join_form">회원가입</Link>
       </div>
     </div>
