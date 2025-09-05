fc27ff54b8de0d1c9b9fbd5b3cb75888c9ec8750
yeonmi0818 <parkyunme@naver.com>
2025-09-05 10:49:26 +0900

    Update page.js

--- src/app/payment_select/page.js
+++ src/app/payment_select/page.js
@@ -32,9 +32,9 @@ export default function SelectPaymentPage() {
         <a href="#" className="back_button">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
+            height="36px"
             viewBox="0 -960 960 960"
-            width="24px"
+            width="36px"
             fill="#222222"
           >
             <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
