62ca7dab02fef2c825eff955ba89ea6d40f00e23
kyt0830 <kyt910830@gmail.com>
2025-09-07 22:05:37 +0900

    feat 채팅, 결제 링크 연결

--- src/app/prod_detail/[id]/page.js
+++ src/app/prod_detail/[id]/page.js
@@ -119,8 +119,8 @@ export default async function ProdDetail({ params }) {
 
           <div className={styles.btn_group}>
             {/* 해당 상품이 로그인된 사용자의 상품이라면 */}
-            <button className={styles.chat}> 채팅하기</button >
-            <button className={styles.pay}> 결제하기</button >
+            <button className={styles.chat}><Link href={`/chat/${product.prod_id}`}>채팅하기</Link></button >
+            <button className={styles.pay}><Link href={`/payment_select/${product.prod_id}`}>결제하기</Link></button >
           </div >
         </div >
 
