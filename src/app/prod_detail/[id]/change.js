c9c884e813601549bbcc4de13297a3c2e2ac8dcc
kyt0830 <kyt910830@gmail.com>
2025-09-11 11:21:19 +0900

    faet 상품 상태 변경 버튼 추가

--- src/app/prod_detail/[id]/change.js
+++ src/app/prod_detail/[id]/change.js
@@ -5,29 +5,24 @@ import { createClient } from '../../../utils/supabase/client';
 export default function Change({ option }) {
   const supabase = createClient();
   const [product, setProduct] = useState(option);
-  console.log(product.prod_id);
-
   const handle = async (evt) => {
     const newStatus = evt.target.value;
-    console.log(newStatus);
-
     const { data, error } = await supabase
       .from("Product")
       .update({ prod_status: newStatus })
       .eq("prod_id", product.prod_id)
+      .single()
       .select()
     if (error) {
       console.error("업데이트 실패:", error.message);
       return;
     }
-    // 상태 업데이트 (중요!)
+    // 상태 업데이트(중요!)
     setProduct((prev) => ({
       ...prev,
       prod_status: newStatus
     }));
-    console.log("업데이트 성공:", data);
-    console.log("업데이트 성공:", product);
-
+    console.log(data);
   };
 
   return (
@@ -36,8 +31,8 @@ export default function Change({ option }) {
         value={product.prod_status}
         onChange={handle}
       >
-        <option value={"1"}>판매중</option>
-        <option value={"0"}>판매완료</option>
+        <option value={1} >판매중</option>
+        <option value={0} >판매완료</option>
       </select>
     </>
   );
